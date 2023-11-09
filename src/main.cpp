5130e0106366b71bdd7e4181e6435b25b1814fc8
Filipp <sap.phil.v@mail.ru>
2023-11-09 13:07:23 +0300

    мелкие правки

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,17 @@ int main()
 
     DEDLIST_DUMP(&dedlist, 0);
 
+    for (int x = 1; x <= 19; x++)
+    {
+        dedlist_delete_tail( &dedlist );
+    }
+
+    DEDLIST_DUMP(&dedlist, 0);
+
+    dedlist_push_head(&dedlist, 42);
+
+    DEDLIST_DUMP(&dedlist, 0);
+
     dedlist_dtor(&dedlist);
 
     return 0;
